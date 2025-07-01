95c4128cc1cb31a1fb13e807e89005f44180b13e
Harddy <kingstonharddycanales@ggmail.com>
2025-07-01 18:51:22 +0800

    installed sanctum

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/awa', function () {
+    return '
+        <h1>Welcome to AWA</h1>
+        <p>This is a simple Laravel applicationsddasdw123.</p>
+        tesadw tae 123xd';
+});
