2268033ea61aff195a233e9d228a7f33e8a34ef8
LeahFaye123 <leahfayegenson1@gmail.com>
2025-08-26 17:03:29 +0800

    Leah-SA-submissions-reports-notification-sysconfig

--- frontend/src/app/(dashboard)/super-admin/submissions/page.tsx
+++ frontend/src/app/(dashboard)/super-admin/submissions/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+
 import React, { useState } from 'react';
 import ProjectCard, { type Project } from '@/components/ui/ProjectCardSub';
 import ArchivedProjectCard, { type ArchivedProject } from '@/components/ui/ArchivedProjectSub';
